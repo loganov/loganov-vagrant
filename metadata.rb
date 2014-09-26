f2da4190d13e5238b18a3ba34b526435590335b6
Gregory Weaver <gregory.weaver@gmail.com>
2014-09-26 09:30:17 -0700

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'loganov-vagrant'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures loganov-vagrant'
+long_description 'Installs/Configures loganov-vagrant'
+version          '0.1.0'
+
+depends          'vagrant'
